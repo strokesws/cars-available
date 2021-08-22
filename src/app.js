79b7acb0346ee4b3ccdb2411dfc3a7e11924573c
Matheus Sartoretto <strokesws@gmail.com>
2021-08-22 01:22:36 +0100

    add per day calc to grid

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ const removeLoader = () => {
   const carGrid = document.querySelector("#car-grid");
   carGrid.classList.remove("hidden");
 };
+
 /*
  * Get list of car availabl
  * @return {array | null} Booking meta data and cars available
@@ -23,6 +24,21 @@ const getCars = async () => {
   } catch (e) {
     throw new Error(err);
   }
+};
+
+/*
+ * Add number of days to booking information
+ * @param {object} booking Booking information
+ * return {object} booking with days
+ */
+const parseBooking = (booking) => {
+  const d1 = new Date(booking["@PickUpDateTime"]);
+  const d2 = new Date(booking["@ReturnDateTime"]);
+
+  const oneDay = 1000 * 60 * 60 * 24;
+  const diffInTime = d2.getTime() - d1.getTime();
+  booking.days = Math.round(diffInTime / oneDay);
+  return booking;
 };
 
 /*
@@ -63,8 +79,9 @@ const sortList = (carList, sortBy) =>
 /*
  * Renders list of cars on the HTML
  * @param {array} carList List of cars to be rendered
+ * @param {object} booking Booking information
  */
-const renderCarList = (carList) => {
+const renderCarList = (carList, booking) => {
   const template = document.querySelector("#car-card-template");
   // To be used in the currency format
   const locale = window.navigator.userLanguage || window.navigator.language;
@@ -105,6 +122,7 @@ const renderCarList = (carList) => {
       .setAttribute("src", Vehicle.PictureURL);
 
     // ESTIMATED TOTAL
+    const totalPerDay = car.TotalCharge["@EstimatedTotalAmount"] / booking.days;
     const currencyOptions = {
       currency: car.TotalCharge["@CurrencyCode"],
       style: "currency",
@@ -112,8 +130,10 @@ const renderCarList = (carList) => {
     const estimatedTotal = new Intl.NumberFormat(
       locale,
       currencyOptions
-    ).format(car.TotalCharge["@EstimatedTotalAmount"]);
-    clone.querySelector(".car-card__value").textContent = estimatedTotal;
+    ).format(totalPerDay);
+    clone.querySelector(
+      ".car-card__value"
+    ).textContent = `${estimatedTotal}/day`;
 
     //VENDOR
     clone.querySelector(".car-card__vendor").textContent = car.vendorName;
@@ -128,8 +148,12 @@ document.addEventListener("DOMContentLoaded", async () => {
   const data = await getCars();
 
   if (!data && !data.length) return removeLoader();
-  const flatCarList = parseCarList(data[0].VehAvailRSCore.VehVendorAvails);
+  const { VehAvailRSCore } = data[0];
+
+  const booking = parseBooking(VehAvailRSCore.VehRentalCore);
+
+  const flatCarList = parseCarList(VehAvailRSCore.VehVendorAvails);
   const sortedList = sortList(flatCarList, "price");
-  renderCarList(sortedList);
+  renderCarList(sortedList, booking);
   removeLoader();
 });
